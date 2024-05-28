48bf3f53ba868e3c3e08e693a0a99c7999c38f30
sppiyush <piyushpkcorp@gmail.com>
2024-05-28 19:35:14 +0530

    Update

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 // we have got the exhibitions data array and we will be putting all the data here 
 
 const exhibitionsData =[{
-    title : "Mumbai Exhibition",
+    title : "Dubai Exhibition",
     category: "mumbai",
-    description:['I got a chance to showcase my skills in Mumbai through an exhibition.']
+    description:['World art Dubai was an experience in itself...appreciation came from all the artists and got a place in their news and magazine.']
 },
 
 {
     title: "Delhi Exhibition",
     category:"delhi",
-    description:['I got a chance to showcase mu skills in Delhi as well']
+    description:['The Delhi exhibition at the India Habitat Centre was always a feature in the cap. All the senior artists were stationed there, so got an opportunity to see them and receive great feedback. It was a really nice experience.']
 },
 
 {
-    title:"Jaipur Exhibition",
+    title:"Mumbai Exhibition",
     category:"jaipur",
-    description:['I got a chance to showcase my skills in Jaipur as well']
+    description:['Mumbai exhibition came with a very fresh beginning and a new experience with the footfall from the Bollywood film industry. Made a few impressions on some hearts was all I gained over there.']
 },
 ]
